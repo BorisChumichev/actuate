596eeef3a9436d9a1a1ee654e81550fc65001c64
Boris Chumichev <boris362@yandex.ru>
2015-10-15 23:21:41 +0300

    Generic control flow implemented, yay

--- index.js
+++ index.js
@@ -4,17 +4,60 @@ var program = require('commander')
   , sp = require('serialport')
   , fs = require('fs')
   , color = require('colors')
+  , message = require('./message')
+  , port = null
 
 program
   .version('0.0.1')
-  .option('-p, --port <path>', 'path to serial port to work with');
+  .option('-p, --port <path>', 'path to serial port to work with')
 
 program
   .command('lp')
   .description('list all availible serial ports')
   .action(listPorts)
 
-program.parse(process.argv);
+program
+  .command('turnon')
+  .description('turn actuator on')
+  .action(turnOn)
+
+program
+  .command('turnoff')
+  .description('turn actuator off')
+  .action(turnOff)
+
+program
+  .command('allow')
+  .description('allow the engine')
+  .action(allowEngine)
+
+program
+  .command('disallow')
+  .description('disallow the engine')
+  .action(disAllowEngine)
+
+program.parse(process.argv)
+
+function initPort(cb) {
+  if (!program.port) {
+    console.error(color.red('path to port is not defined'))
+    process.exit(1)
+  }
+  try {
+    var sp = new slipper(program.port, {
+      baudrate: 115200
+    })
+  } catch (err) {
+    handleError(err)
+  }
+  sp.on('error', function (err) {
+    handleError(err)
+  })
+  sp.on('open', function () {
+    port = sp
+    cb()
+  })
+}
 
 function listPorts () {
   sp.list(function (err, ports) {
@@ -22,4 +65,42 @@ function listPorts () {
       console.log(color.green(port.comName))
     })
   })
+}
+
+function turnOn () {
+  initPort(function () {
+    port.write(message.command(0x01), handleWriteOnce)
+  })
+}
+
+function turnOff () {
+  initPort(function () {
+    port.write(message.command(0x00), handleWriteOnce)
+  })
+}
+
+function allowEngine () {
+  initPort(function () {
+    port.write(message.command(0x15), handleWriteOnce)
+  })
+}
+
+function disAllowEngine () {
+  initPort(function () {
+    port.write(message.command(0x20), handleWriteOnce)
+  })
+}
+
+function handleWriteOnce(err, res) {
+  if (err) {
+    handleError(err)
+  } else {
+    console.log(color.green('done, ' +res+ ' bytes sent'))
+    process.exit(0)
+  }
+}
+
+function handleError(err) {
+  console.error(color.red(err))
+  process.exit(1)
 }
---
+++ message.js
@@ -0,0 +1,22 @@
+var m = new Buffer([ 0x80, 0x80, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00 ])
+  , pa = 3
+  , ca = 5
+  , sa = 6
+  , noop = 0x03
+
+module.exports = {
+  command: function (code) {
+    m.writeUInt8(code, ca)
+    return m
+  },
+  position: function (number) {
+    m.writeUInt8(noop, ca)
+    m.writeInt16LE(parseInt(number*10), pa)
+    return m
+  },
+  state: function (number) {
+    m.writeUInt8(noop, ca)
+    m.writeInt16LE(number, sa)
+    return m
+  }
+}
